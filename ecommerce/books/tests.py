28c7a3c94dd3b7d0110adfc63601790d3b71d45e
milan gohil <milangoh82@gmail.com>
2025-09-20 22:59:29 +0530

    cmom

--- ecommerce/books/tests.py
+++ ecommerce/books/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+class BookModelTest(TestCase):
+    def setUp(self):
+        Book.objects.create(title='Test Book', author='Test Author', price=10.00)
+
+    def test_book_creation(self):
+        book = Book.objects.get(title='Test Book')
+        self.assertEqual(book.price, 10.00)
